9b0c2eb01f28e8df22418d13bd5bbd90c989651b
Gunther Cox <gunthercx@gmail.com>
2017-09-07 01:27:46 -0400

    Configure AJAX safe methods and CSRF

---
+++ django_bootstrap_base_template/static/js/csrf.js
@@ -0,0 +1,13 @@
+var csrftoken = Cookies.get('csrftoken');
+
+function csrfSafeMethod(method) {
+    // These HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader('X-CSRFToken', csrftoken);
+        }
+    }
+});
