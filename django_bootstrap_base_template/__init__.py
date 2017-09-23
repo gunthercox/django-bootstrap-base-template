98ff57bbf636e3f55b9515f076411797a128fac3
Gunther Cox <gunthercx@gmail.com>
2017-09-23 19:33:37 -0400

    Update version to 0.0.2

--- django_bootstrap_base_template/__init__.py
+++ django_bootstrap_base_template/__init__.py
@@ -2,7 +2,7 @@
 A base template for Django projects using Bootstrap.
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
 __url__ = 'https://github.com/gunthercox/django-bootstrap-base-template'
