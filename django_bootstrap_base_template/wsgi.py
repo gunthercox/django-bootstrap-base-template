2e3da5d831442143600cd47f3558a332f4c90f45
Gunther Cox <gunthercx@gmail.com>
2018-01-21 09:47:05 -0500

    Wrap line to fix linting errors

--- django_bootstrap_base_template/wsgi.py
+++ django_bootstrap_base_template/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_bootstrap_base_template.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "django_bootstrap_base_template.settings"
+)
 
 application = get_wsgi_application()
